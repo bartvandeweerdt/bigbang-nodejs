7de169b0aa0eb344c3a8a61ad3464eba8a81850d
Bart Vandeweerdt <bart.vandeweerdt@ida-mediafoundry.be>
2014-01-14 10:40:12 +0100

    no message

--- app.js
+++ app.js
@@ -4,33 +4,36 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
-var person = require('./routes/person');
 var http = require('http');
 var path = require('path');
+var routes = require('./routes'); // if there is no package.json file present in the directory, then node will attempt to load an index.js or index.node file out of that directory.
+var person = require('./routes/person');
 
-var app = express();
+var app = express(); // creates a new application
 
 // all environments
 app.set('port', process.env.PORT || 5000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
+app.set('views', path.join(__dirname, 'views')); // the view directory path, defaulting to "./views"
+app.set('view engine', 'ejs'); // the default engine extension to use when omitted || Embedded JavaScript
+
+app.use(express.favicon()); // setup favicon. Not passing a path will use the default express favicon
+app.use(express.logger('dev')); // log every request (http://www.senchalabs.org/connect/logger.html)
+app.use(express.json()); // http://www.senchalabs.org/connect/json.html
+app.use(express.urlencoded()); // http://www.senchalabs.org/connect/urlencoded.html
+app.use(express.methodOverride()); // http://www.senchalabs.org/connect/methodOverride.html
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'))); // http://www.senchalabs.org/connect/static.html
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+  app.use(express.errorHandler()); // http://www.senchalabs.org/connect/errorHandler.html
 }
 
 app.get('/', routes.index);
 app.get('/persons', person.list);
+app.get('/persons/:personId', person.get);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
--- routes/person.js
+++ routes/person.js
@@ -7,6 +7,23 @@ var Person = require('../models/Person');
 exports.list = function (req, res) {
     Person.find({}, function(err, persons) {
         if (err) return next(err);
-        res.send(persons);
+
+        //res.send(persons);
+
+        res.jsonp(200, persons);
+
     });
 };
+
+exports.get = function (req, res) {
+    var personId = req.params.personId;
+
+    Person.findOne({"id":personId}, function(err, person) {
+        if (err) return next(err);
+
+        //res.send(persons);
+
+        res.jsonp(200, person);
+
+    });
+}
